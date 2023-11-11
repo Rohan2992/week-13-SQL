39676cfdbf713eda500f37f0252c2ca2beccfda3
Rohan2992 <rohan.chaudhary29.rc@gmail.com>
2023-11-12 00:05:59 +0530

    updated uri

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ import { Client } from "pg";
 
 export async function getClient() {
   const client = new Client(
-    "postgres://aymciypq:VP7h35Y0A6SK4deKc65Q3_PkszGLjC4P@bubble.db.elephantsql.com/aymciypq"
+    "postgres://aymciypq:***@bubble.db.elephantsql.com/aymciypq"
   );
   await client.connect();
   return client;
